27993d3b4271d85d2839c26815a9c8a4ee91df68
mr.Shu <mr@shu.io>
2015-02-26 01:17:33 +0100

    update: Directory structure, random strings
    * Changed directory structure a bit.
    
    * Added functions that deal with random strings.
    
    Signed-off-by: mr.Shu <mr@shu.io>

--- blackbox.pl
+++ blackbox.pl
@@ -15,14 +15,28 @@ sub c {
     close $h;
 }
 
-my @alp = ('a' .. 'z');
+sub rmaker {
+    return sub {
+        rt(rand(3) + 1)
+    };
+}
 
+my $n = rmaker();
+
+my @alp = ('a' .. 'z');
+my $l = ord (substr (&$n, 1, 1)) % 26;
 for my $x (@alp) {
     mkdir "$x";
     for my $y (@alp) {
         mkdir "$x/$x$y";
         for my $a (1..15) {
-            c("$x/$x$y/$x$y-subor$a", rt((rand(3) + 1)));
+            c("$x/$x$y/$x$y-subor$a", &$n);
+            next unless $y eq $alp[$l];
+            c("$x/$x$y/-subor$a.$x$y", &$n);
+            next unless $y eq $alp[($l+rand(5))%26] and $y eq $alp[$l];
+            c("$x/$x$y/--subor$a.$x$y", &$n);
         }
     }
 }
+
+c('e/eh/.p', rt(100) . $ENV{USER} . rt(100));
