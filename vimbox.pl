98b68c2ef9178c8e43180fdb751665d1f9eab327
Peter Polacik <pepol@imterra.com>
2015-03-30 23:36:51 +0200

    update: Added vimgolf examples.
    Signed-off-by: Peter Polacik <polacik.p@gmail.com>

--- vimbox.pl
+++ vimbox.pl
@@ -78,3 +78,68 @@ As you no doubt realize by now, Perl has some really odd features, and the DATA 
 ---------- 10
 As you no doubt realize by now, has some really odd features, and the DATA file handle is one of them. This file handle lets you store read-only data in the same file as your Perl script, which might come in handy if you need to send both code and data to someone via e-mail.
 ==========
+Moist von Lipwig | 10.14
+Reacher Gilt | 15.34
+Patrician Havelock Vetinari | 6.73
+Adora Belle Dearheart | 13
+Mr Pump | 7.3
+Mr Spools | 17.89
+Drumknott | 20.30
+Tolliver Groat | 14.56
+Miss Cripslock | 8.92
+Angua | 20.1
+Mr Gryle | 53.42
+Stanley Howler | 25.67
+Horsefry | 0.43
+Postman | 1.58
+---------- 100
+1. ucastnik: Horsefry | 0.43
+2. ucastnik: Postman | 1.58
+3. ucastnik: Patrician Havelock Vetinari | 6.73
+4. ucastnik: Mr Pump | 7.3
+5. ucastnik: Miss Cripslock | 8.92
+6. ucastnik: Moist von Lipwig | 10.14
+7. ucastnik: Adora Belle Dearheart | 13
+8. ucastnik: Tolliver Groat | 14.56
+9. ucastnik: Reacher Gilt | 15.34
+10. ucastnik: Mr Spools | 17.89
+11. ucastnik: Angua | 20.1
+12. ucastnik: Drumknott | 20.30
+13. ucastnik: Stanley Howler | 25.67
+14. ucastnik: Mr Gryle | 53.42
+==========
+Moist von Lipwig | 10.14
+Reacher Gilt | 15.34
+Patrician Havelock Vetinari | 6.73
+
+Adora Belle Dearheart | 13
+Mr Pump | 7.3
+Mr Spools | 17.89
+Drumknott | 20.30
+Tolliver Groat | 14.56
+
+Miss Cripslock | 8.92
+Angua | 20.1
+
+Mr Gryle | 53.42
+
+Stanley Howler | 25.67
+Horsefry | 0.43
+
+Postman | 1.58
+---------- 90
+1. Horsefry | 0.43
+2. Postman | 1.58
+3. Patrician Havelock Vetinari | 6.73
+4. Mr Pump | 7.3
+5. Miss Cripslock | 8.92
+6. Moist von Lipwig | 10.14
+7. Adora Belle Dearheart | 13
+8. Tolliver Groat | 14.56
+9. Reacher Gilt | 15.34
+10. Mr Spools | 17.89
+11. Angua | 20.1
+12. Drumknott | 20.30
+13. Stanley Howler | 25.67
+14. Mr Gryle | 53.42
+==========
